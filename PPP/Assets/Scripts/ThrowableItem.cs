86d2fc7ef459e99bbcdb16332b76eaad3d74f456
Adriano Maddog <alk.gordo@gmail.com>
2015-06-18 16:39:23 -0300

    Fase 3 iniciada

---
+++ PPP/Assets/Scripts/ThrowableItem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowableItem : MonoBehaviour {
+    // Atributos:
+    private float lapse; // Instante de tempo salvo;
+    private float counter = 15; // Tempo total vivo;
+    //private Rigidbody2D body;
+    //private float fX = 400.0f, fY = 400.0f; // Força em X e em Y;
+    
+    //public float forcaX{
+    //    set{
+    //        this.fX = value;
+    //    }
+    //    get{
+    //        return this.fX;
+    //    }
+    //}
+    //public float forcaY
+    //{
+    //    set
+    //    {
+    //        this.fY = value;
+    //    }
+    //    get
+    //    {
+    //        return this.fY;
+    //    }
+    //}
+
+    // Métodos:
+    void Start()
+    {
+        //// Pegando gameObject:
+        //body = this.gameObject.GetComponent<Rigidbody2D>();
+
+        //// Aplicando forças:
+        //body.AddForce(new Vector2(fX, fY));
+        //body.AddTorque(100f);
+
+        lapse = Time.time;
+    }
+
+    void Update(){
+
+        // Auto destruição após tempo X:
+        if(Time.time >= lapse + counter){
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ PPP/Assets/Scripts/Thrower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thrower : MonoBehaviour {
+
+    // Atributos:
+    public GameObject[] prefabs;
+    public Vector2 releasePoint;
+    public float fx = 400.0f, fy = 400.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.releasePoint = this.gameObject.transform.position; // Default é o centro;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Rigidbody2D body = ((GameObject)Instantiate(prefabs[0], releasePoint, Quaternion.identity)).gameObject.GetComponent<Rigidbody2D>();
+            //Rigidbody2D body = o.gameObject.GetComponent<Rigidbody2D>();
+
+            // Aplicando forças:
+            body.AddForce(new Vector2(fx, fy));
+            body.AddTorque(100f);
+        }	
+	}
+}
